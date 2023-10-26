443163274a8237d6339db82f2522ca8b8e45b2f7
Lashe <adeola5678@gmail.com>
2023-10-26 20:22:33 +0100

    fix: update background image

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,18 @@
-import './globals.css'
+import './globals.css';
 
-import type { AppProps } from 'next/app'
-import Providers from './providers'
-import Head from 'next/head'
-import { Poppins } from 'next/font/google'
+import type { AppProps } from 'next/app';
+import Providers from './providers';
+import Head from 'next/head';
 //import { env } from '@/config/environment'
 //import { getDeployments } from '@/deployments/deployments'
 
 // NOTE: This may cause CORS errors during the development
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
+// const poppins = Poppins({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-poppins',
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -22,8 +21,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>RejuvenateAI</title>
       </Head>
       <Providers>
-        <Component {...pageProps} className={poppins.className} />
+        <Component {...pageProps} />
       </Providers>
     </>
-  )
+  );
 }
