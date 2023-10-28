591e2dcddddb5d8352bf84f198650d1ab2e0c552
elcharitas <jonathanirhodia@gmail.com>
2023-10-28 09:25:50 +0100

    wrap up treasury

--- contracts/src/treasury/lib.rs
+++ contracts/src/treasury/lib.rs
@@ -1,9 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[openbrush::implementation(PSP22, PSP22Ownable)]
+#[openbrush::implementation(Ownable, PSP22)]
 #[openbrush::contract]
 pub mod treasury {
-    use openbrush::traits::{Storage, String};
+    use openbrush::{
+        contracts::{ownable::OwnableError, psp22::PSP22Impl},
+        modifiers,
+        traits::Storage,
+    };
 
     #[ink(storage)]
     #[derive(Default, Storage)]
@@ -16,14 +20,20 @@ pub mod treasury {
 
     impl Contract {
         #[ink(constructor)]
-        pub fn new(initial_supply: Balance) -> Self {
+        pub fn new() -> Self {
             let mut _instance = Self::default();
-            psp22::Internal::_mint(&mut _instance, Self::env().caller(), initial_supply)
-                .expect("Should mint");
             ownable::Internal::_init_with_owner(&mut _instance, Self::env().caller());
             _instance
         }
 
-        pub fn withdrawFunds(self, amount: u128, address: String) {}
+        #[modifiers(only_owner)]
+        pub fn withdraw_funds(
+            &mut self,
+            amount: Balance,
+            address: AccountId,
+        ) -> Result<(), OwnableError> {
+            let _ = PSP22Impl::transfer(self, address, amount, Vec::new());
+            Ok(())
+        }
     }
 }
